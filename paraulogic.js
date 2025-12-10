22cabbc2c30134b60bc4583c50e315dd4c781f56
Javi <javierrodriguezv@alumnat.iesdamiahuguet.net>
2025-12-10 09:40:51 +0100

    Mejora de diseño

--- paraulogic.js
+++ paraulogic.js
@@ -1,17 +1,17 @@
-const secreta = 'RADIO'
+const secreta = 'CALVO'
 let resuelto = false
 let intentos = 0
 /*
 Inicialitzam valors
  */
 //document.getElementById('pista').value='La paraula d\'avui és de :'+secreta.length + ' lletres.'
-function analitzarParaula() {
-    let palabra = document.getElementById('resposta').value.toUpperCase();
+function analitzarParaula(prueba) {
+    let palabra = prueba.toUpperCase()
     let colores = ""
     if (intentos < 5) {
         if (palabra.length === 5) {
             if (palabra === secreta) {
-                document.getElementById('enviar').disabled = true
+                // document.getElementById('enviar').disabled = true
                 resuelto = true
             } else {
                 for (let i = 0; i < palabra.length; i++) {
@@ -28,13 +28,16 @@ function analitzarParaula() {
             }
             intentos++
             pintarResposta(palabra, colores)
+            if (intentos >= 5){
+                document.getElementById('title').innerHTML = '<h1>PERDEDOR PRINGADO</h1>'
+            }
         } else {
             alert("palabra de 5 letras pls")
         }
 
     }
     else {
-        document.getElementById('enviar').disabled = true
+        // document.getElementById('enviar').disabled = true
         alert("Te has quedado sin intentos. PERDEDOR")
     }
 }
@@ -53,7 +56,7 @@ function pintarResposta(paraula, colores) {
             final += '        <div class="slot">' + paraula.charAt(i) + '</div>\n'
         }
         else if (colores.charAt(i) === "1") {
-            final += '        <div class="slot yellow">' + paraula.charAt(i) + '</div>\n'
+            final += '        <div class="slot blue">' + paraula.charAt(i) + '</div>\n'
         }
         else {
             final += '        <div class="slot green">' + paraula.charAt(i) + '</div>\n'
@@ -62,8 +65,22 @@ function pintarResposta(paraula, colores) {
     final += '</div>'
     document.getElementById('respostes').innerHTML += final
 }
-document.addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && resuelto === false) {
-        analitzarParaula();
+
+let texto = "";
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace") {
+        texto = texto.slice(0, -1);
     }
+    else if (event.key === "Enter" && resuelto === false) {
+        analitzarParaula(texto);
+        texto = ''
+    }
+    if (texto.length < 5) {
+        if (event.key.length === 1) {
+            texto += event.key;
+        }
+    }
+
+    document.getElementById("texto").textContent = texto;
 });
